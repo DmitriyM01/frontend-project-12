a301b53c4aa0828a05a17d31fb0e34e3fc31f7e5
Dimitro <mironowd697@gmail.com>
2024-09-20 15:54:52 -0500

    qfix

--- frontend/src/schemas/schemas.js
+++ frontend/src/schemas/schemas.js
@@ -3,13 +3,13 @@ import * as yup from 'yup';
 export const signupSchema = yup.object().shape({
   username: yup.string()
     .matches(/^[a-z\d]+$/gi, 'latin')
-    .min(3, 'Минимум 3 символа')
-    .max(20, 'Максимум 20 символов')
+    .min(3, 'От 3 до 20 символов')
+    .max(20, 'От 3 до 20 символов')
     .required("Это обязательное поле"),
   password: yup.string()
     .matches(/^[a-z\d@$!%*#?&]+$/gi, 'validPassword')
-    .min(6, 'Минимум 6 символов')
-    .max(20, 'Максимум 20 символов')
+    .min(6, 'От 6 до 20 символов')
+    .max(20, 'От 6 до 20 символов')
     .required("Это обязательное поле"),
-  confirmPassword: yup.string().required("Это обязательное поле").oneOf([yup.ref('password'), null], 'Пароль должен совпадать'),
+  confirmPassword: yup.string().required("Это обязательное поле").oneOf([yup.ref('password'), null], 'Пароли должен совпадать'),
 });
