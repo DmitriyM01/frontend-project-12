b172c0eab0a7effe730e5dd26312e851c05eec1b
Dimitro <mironowd697@gmail.com>
2024-09-20 21:56:43 -0500

    qfix

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -46,7 +46,13 @@ const Channels = ({ currentChannel }) => {
 
                 <div key={channel.id} className="btn-group d-grid gap-2">
                   <ButtonGroup>
-                    <Button onClick={() => dispatch(channelsActions.setCurrentChannel(channel))} variant={currentChannel.id === id ? 'secondary' : ''}><span class="me-1"># </span>{channel.name}</Button>
+                    <Button 
+                      onClick={() => dispatch(channelsActions.setCurrentChannel(channel))} 
+                      variant={currentChannel.id === id ? 'secondary' : ''}
+                    >
+                      <span class="me-1"># </span>
+                      {channel.name}
+                    </Button>
 
                     {
                       channel.removable ?
